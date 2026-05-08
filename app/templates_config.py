9c23b489989152972a9591bc057104b055503547
Rodry03 <03.rodry.03@gmail.com>
2026-05-08 19:10:42 +0200

    control de sesiones, hora de madrid

--- app/templates_config.py
+++ app/templates_config.py
@@ -1,4 +1,8 @@
+from datetime import datetime, timezone
+from zoneinfo import ZoneInfo
 from fastapi.templating import Jinja2Templates
+
+_MADRID = ZoneInfo("Europe/Madrid")
 
 
 def _get_flashes(request) -> list:
@@ -8,6 +12,15 @@ def _get_flashes(request) -> list:
     return flashes
 
 
+def _to_madrid(dt: datetime) -> datetime:
+    if dt is None:
+        return None
+    if dt.tzinfo is None:
+        dt = dt.replace(tzinfo=timezone.utc)
+    return dt.astimezone(_MADRID)
+
+
 templates = Jinja2Templates(directory="app/templates")
 templates.env.globals["enumerate"] = enumerate
 templates.env.globals["get_flashes"] = _get_flashes
+templates.env.filters["madrid"] = _to_madrid
